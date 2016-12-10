4f960f497e53812661969c631a29a072e759d2a6
jakob <jakob.braun@posteo.de>
2016-12-10 15:39:35 +0100

    uipdaed loader.sh in oder to fit non-login ical providers

--- loader.sh
+++ loader.sh
@@ -4,7 +4,14 @@ if [ -z $ical_url ]; then echo "please set \$ical_url";
 exit;
 fi;
 
-if [ -z $ical_user ]; then echo "please set \$ical_user"; 
+dir=./
+
+if [ -z $ical_user ]; then 
+echo "no user specifyed. trying without auth."; 
+
+curl $ical_url  > $dir'MYical.ics'
+php generateList.php
+ 
 exit;
 fi;
 
@@ -12,7 +19,6 @@ if [ -z $ical_pw ]; then echo "please set \$ical_pw";
 exit;
 fi;
 
- dir=./
+
  curl $ical_url  -u$ical_user:$ical_pw  > $dir'MYical.ics'
- 
  php generateList.php
